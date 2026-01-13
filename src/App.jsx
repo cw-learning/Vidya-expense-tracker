840f88ffbdfe22f3ad79cc53aaef2ef1e04e0b31
vidyashreebv <vidyashreebv11@gmail.com>
2026-01-13 17:52:28 +0530

    feat(setup): initialize react app using vite

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black">
+      <p className="text-white text-lg">expense tracker app</p>
+    </main>
+  );
+}
